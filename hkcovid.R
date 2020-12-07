c87daa27eab900eecdb35d421d5469e3e76e1298
Raymond Lowe <raymondclowe@gmail.com>
2020-12-07 13:33:13 +0000

    as one line+

--- hkcovid.R
+++ hkcovid.R
@@ -6,6 +6,7 @@ hkdeceased <- hkcovid %>% filter(Hospitalised.Discharged.Deceased == "Deceased")
 summary(hkdeceased)
 hkcovid %>% filter(Hospitalised.Discharged.Deceased == "Discharged") %>% ggplot(.) + aes(Age, color = Gender) + geom_bar()
 hkcovid <- mutate(hkcovid, Onset.Date.Date = parse_date(Date.of.onset, "%d/%m/%Y"))
+hkcovid <- mutate(hkcovid, Onset.Date.Date = parse_date(Date.of.onset, "%d/%m/%Y"))
 hkcovid %>% filter(Hospitalised.Discharged.Deceased == "Discharged") %>% ggplot(.) + aes(Onset.Date.Date, color = Gender) + geom_bar()
 ggplot(hkcovid) + aes(Report.Date.Date, color = Gender) + geom_bar()
 hkcovid <- mutate(hkcovid, lower.hk.resident = tolower(HK.Non.HK.resident))
@@ -14,7 +15,7 @@ hkcovidagemodel <- lm(hkcovid$Age ~ hkcovid$Case.no.)
 plot(hkcovidagemodel)
 
 hkcovid <- mutate(hkcovid, Report.Date.Date = parse_date(Report.date, "%d/%m/%Y"))
-hkcovid <- mutate(hkcovid, Onset.Date.Date = parse_date(Date.of.onset, "%d/%m/%y"))
+#hkcovid <- mutate(hkcovid, Onset.Date.Date = parse_date(Date.of.onset, "%d/%m/%y"))
 hkcovid <- mutate(hkcovid, lower.hk.resident = tolower(HK.Non.HK.resident))
 hkcovid$lower.hk.resident <- as.factor(hkcovid$lower.hk.resident)
 hkcovid$Hospitalised.Discharged.Deceased <- as.factor(hkcovid$Hospitalised.Discharged.Deceased)
@@ -28,3 +29,17 @@ hkcovid %>% group_by(Report.Date.Date, Gender) %>% add_tally() %>% ggplot(.) + a
 
 # prediction
 hkcovid %>% group_by(Report.Date.Date, Gender) %>% add_tally() %>% ggplot(.) + aes(y = n, x = Report.Date.Date) + geom_line() + geom_smooth(fullrange = TRUE) + scale_y_continuous(limit=c(10,NA),oob=squish) + scale_x_date(limits = c(as.Date("2020-01-02"), as.Date("2022-06-02")))
+
+
+library('scales')
+# in one line
+as_tibble(read.csv('http://www.chp.gov.hk/files/misc/enhanced_sur_covid_19_eng.csv')) %>% 
+  mutate(Report.Date.Date = parse_date(Report.date, "%d/%m/%Y")) %>% 
+  group_by(Report.Date.Date, Gender) %>%
+  add_tally() %>%
+  ggplot(.) + 
+    aes(y = n, x = Report.Date.Date) + 
+    geom_line() + 
+    geom_smooth(fullrange = TRUE) + 
+    scale_y_continuous(limit=c(10,NA),oob=squish) + 
+    scale_x_date(limits = c(as.Date("2020-01-02"), as.Date("2020-12-31")))
