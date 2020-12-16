b4672ce3dd8dc5a41bcc0784441c2d7ecb75a9f5
Raymond Lowe <raymondclowe@gmail.com>
2020-12-16 11:30:26 +0000

    sutff

--- hkcovid.R
+++ hkcovid.R
@@ -35,12 +35,29 @@ library('scales')
 # in one line
 as_tibble(read.csv('http://www.chp.gov.hk/files/misc/enhanced_sur_covid_19_eng.csv')) %>% 
   mutate(Report.Date.Date = parse_date(Report.date, "%d/%m/%Y")) %>% 
-  group_by(Report.Date.Date, Gender) %>%
+  group_by(Report.Date.Date) %>%
   add_tally() %>%
   ggplot(.) + 
-    aes(y = n, x = Report.Date.Date, color = Gender) + 
-    geom_jitter() + 
+    aes(y = n, x = Report.Date.Date) + 
+    geom_point() + 
     geom_smooth(fullrange = TRUE) + 
     scale_y_continuous(limit=c(1,NA),oob=squish) + 
-    scale_x_date(limits = c(as.Date("2020-01-01"), as.Date("2020-12-12")))
+    scale_x_date(limits = c(as.Date("2020-11-01"), as.Date("2020-12-16")))
+
+# in one line nov 1 until  today
+mycolors <- c("Cyan2")
+Clusters <- c("Dance Studio")
+
+as_tibble(read.csv('http://www.chp.gov.hk/files/misc/enhanced_sur_covid_19_eng.csv')) %>% 
+  mutate(Report.Date.Date = parse_date(Report.date, "%d/%m/%Y")) %>% 
+  group_by(Report.Date.Date) %>%
+  add_tally() %>%
+  ggplot(.) + 
+  aes(y = n, x = Report.Date.Date) + 
+  geom_point() + 
+  geom_smooth(fullrange = TRUE) + 
+  scale_y_continuous(limit=c(1,NA),oob=squish) + 
+  scale_x_date(limits = c(as.Date("2020-11-01"), Sys.Date())) +
+  geom_text(aes(label=ifelse(Report.Date.Date == as.Date("2020-11-20"),"Dance Studio Cluster",""), col=Clusters, hjust=0,vjust=-5)) +
+  scale_color_manual(values=mycolors)
 
